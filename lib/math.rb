91c71adcadd56fea999849719c757842041ad235
Meagan Utsey <meagan.utsey@gmail.com>
2019-08-07 03:21:29 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,32 @@
 def addition(num1, num2)
-
+1+2 
 end
+
+a_method(1,2) #=> 3
 
 def subtraction(num1, num2)
-
+1-2
 end
+
+a_method(1,2) #=> -1
 
 def division(num1, num2)
-
+4/2
 end
+
+a_method(4,2) #=> 2 
 
 def multiplication(num1, num2)
-
+1*2 
 end
+a_method(1,2) #=> 2 
 
 def modulo(num1, num2)
-
+5mod3
 end
+a_method(5,3) #=> 2 
 
 def square_root(num)
-
+4=|4|
 end
+a_method(4,4) #=> 16
